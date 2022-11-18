170af29f07cf95bb4371f2fb9b9f652c08b69520
MasashiIde <117148113+MasashiIde@users.noreply.github.com>
2022-11-18 13:00:41 +0000

    [Update]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :users,only: [:show,:edit]
+  resources :users,only: [:index,:show,:edit]
   resources :books,only: [:new,:index,:show,:edit]
 
   get "/home/about"=> "homes#about",as:"about"
